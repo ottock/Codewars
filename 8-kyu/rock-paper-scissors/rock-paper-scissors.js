2e206dab4627254a9eab1e6ad3d9520a7c1110b4
ottock <86604082+ottock@users.noreply.github.com>
2026-04-16 22:05:15 -0300

    Add solution - CodeHub

---
+++ 8-kyu/rock-paper-scissors/rock-paper-scissors.js
@@ -0,0 +1,34 @@
+const rps = (p1, p2) => {
+  switch (p1) {
+    case "rock":
+      if(p2 == "rock"){
+        return "Draw!";
+      } else if (p2 == "paper"){
+        return "Player 2 won!";
+      } else{
+        return "Player 1 won!";
+      }
+​
+    case "paper":
+      if(p2 == "rock"){
+        return "Player 1 won!";
+      } else if(p2 == "paper"){
+        return "Draw!";
+      } else{
+        return "Player 2 won!";
+      }
+​
+    case "scissors":
+      if(p2 == "rock"){
+        return "Player 2 won!";
+      }
+      else if(p2 == "paper"){
+        return "Player 1 won!";
+      }
+      else{
+        return "Draw!";
+      }
+  }
+​
+  return "Invalid input";
+};
